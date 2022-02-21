4f9d40c644a7972a2ab9c6c06754d05d128e12b9
unknown <rodriguess.lucass@gmail.com>
2022-02-21 17:32:46 -0300

    nomodule found

--- livrosApp/livrosApp/settings.py
+++ livrosApp/livrosApp/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'livrosApp',
+    'livrosApp.livrosApp.settings',
     'meusLivrosApp.apps.MeuslivrosappConfig',
 ]
 
