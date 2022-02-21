b4d923b34dce44578982fa3a7774338b3685fd6f
unknown <rodriguess.lucass@gmail.com>
2022-02-21 17:38:12 -0300

    installed_apps2

--- livrosApp/livrosApp/settings.py
+++ livrosApp/livrosApp/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'livrosApp',
-    'livrosApp.meusLivrosApp.apps.MeuslivrosappConfig',
+    'meusLivrosApp',
+    #  'meusLivrosApp.apps.MeuslivrosappConfig',
     #  'meusLivrosApp.apps.MeuslivrosappConfig',
 ]
 
