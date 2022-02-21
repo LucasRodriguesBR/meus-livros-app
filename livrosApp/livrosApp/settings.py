576d97a403ebdf2e03805035eb4530de2720f0bf
unknown <rodriguess.lucass@gmail.com>
2022-02-21 17:33:57 -0300

    erro

--- livrosApp/livrosApp/settings.py
+++ livrosApp/livrosApp/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'livrosApp.livrosApp.settings',
+    'livrosApp.settings',
     'meusLivrosApp.apps.MeuslivrosappConfig',
 ]
 
