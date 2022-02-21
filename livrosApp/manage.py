e6ed91e3c3661f81df0d2b074fc02ede522b366c
unknown <rodriguess.lucass@gmail.com>
2022-02-21 18:26:24 -0300

    manage.py

--- livrosApp/manage.py
+++ livrosApp/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'livrosApp.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'livrosApp.livrosApp.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
